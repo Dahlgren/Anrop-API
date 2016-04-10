a8a00a0a8ada8689233faea60c591c2d189dc17f
Björn Dahlgren <bjorn@dahlgren.at>
2016-04-10 19:07:52 +0200

    Include user avatar url with user response objects

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,23 @@ class User < ApplicationRecord
   alias_attribute :email, :user_email
   alias_attribute :algorithm, :user_algo
   alias_attribute :salt, :user_salt
+  alias_attribute :avatar, :user_avatar
 
   has_many :shouts, foreign_key: "shout_name"
+
+  AVATAR_BASE_URL = "https://www.anrop.se/images/avatars/"
 
   ROLES = {
     operations: '7',
   }
+
+  def avatar_url
+    if avatar and avatar.length > 0
+      "#{AVATAR_BASE_URL}#{avatar}"
+    else
+      "#{AVATAR_BASE_URL}noavatar100.png"
+    end
+  end
 
   def authenticate_hash(expiration)
     digest = OpenSSL::Digest.new(self.algorithm)
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attribute :id
   attribute :name
+  attribute :avatar_url
 end
